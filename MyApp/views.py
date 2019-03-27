d2f464fe2fad15ee417fd1652b041ac9e97bf351
Eddy V <edva59012@outlook.com>
2019-03-27 07:17:44 -0600

    necessary imports specified for restful api

--- MyApp/views.py
+++ MyApp/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonREsponse
+from django.core import serializers
+from django.conf import settings
+import json
 # Create your views here.
